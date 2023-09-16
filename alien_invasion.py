b73ce14c4cfa1b17e89c52b17f549a1b4d77b3da
Ilya Sukhadolets <vega334@gmail.com>
2023-09-16 14:46:05 +0300

    Создание файла с реcурсами и который запускает окно игры

---
+++ alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """ Класс для управления ресурсами и поведением игры."""
+    def __init__(self):
+        """ Инициалищирует игру и созает русурсы."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+    
+    def run_game(self):
+        """ Запуск основного цикла игры."""
+        while True:
+            # Отслеживание событий клавиатуры и мыши.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # Отображение последнего прорисованного экрана
+            pygame.display.flip()
+if __name__ == "__main__":
+    # Создание экземпляра и запуск игры
+    ai = AlienInvasion()
+    ai.run_game()
