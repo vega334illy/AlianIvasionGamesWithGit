693b909d1b98b3f4a241f569a3d77739617ed55b
Ilya Sukhadolets <vega334@gmail.com>
2023-09-18 17:00:04 +0300

    add: game right, left key events add: ship right, left movies

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,11 +16,13 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+
     
     def run_game(self):
         """ Запуск основного цикла игры."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -29,7 +31,19 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-    
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Muve ship on the right.
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+
     def _update_screen(self):
         """ Update screen."""
         # Get background.
--- ship.py
+++ ship.py
@@ -10,6 +10,15 @@ class Ship():
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """ Update ship position."""
+        if self.moving_right:
+            self.rect.x += 1
+        elif self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
