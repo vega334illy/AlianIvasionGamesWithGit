a85bb2ecd2a75fe8c1e2aaa59461213a87c18726
Ilya Sukhadolets <vega334@gmail.com>
2023-09-21 23:59:31 +0300

    ref: game add _check_keyup_event(), _check_keydown_event() functions

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,17 +32,22 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # Muve ship on the right.
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+    
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+        # Muve ship on the right.
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
 
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """ Update screen."""
@@ -53,11 +58,7 @@ class AlienInvasion:
         # Отображение последнего прорисованного экрана.
 
         pygame.display.flip()
-    
-
-    
-
-
+ 
 if __name__ == "__main__":
     # Создание экземпляра и запуск игры
     ai = AlienInvasion()
