e55501b92d10ecc3703dc948fd03f24ab60f5b87
Ilya Sukhadolets <vega334@gmail.com>
2023-09-26 13:44:09 +0300

    add: fullscreen window fix: bug quit event, key 'q'

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,8 +11,9 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
@@ -42,7 +43,7 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-        elif self.key == pygame.K_q:
+        elif event.key == pygame.K_q:
             sys.exit()
 
     def _check_keyup_events(self, event):
