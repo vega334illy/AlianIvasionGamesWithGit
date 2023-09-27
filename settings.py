27b2d37e9bcfdc07c9496813d0463f7c77f33bb2
Illia Sukhadolets <vega334@gmail.com>
2023-09-27 13:02:30 +0300

    add: bullet settings to settings.py

--- settings.py
+++ settings.py
@@ -8,3 +8,9 @@ class Settings():
         self.bg_color = (230, 230, 230)
         # Ship settings
         self.ship_speed = 1.5
+        # Bullet settings
+        self.bullet_speed = 1 
+        self.bullet_width = 3 
+        self.bullet_height = 15 
+        self.bullet_color = (60, 60, 60)
+
