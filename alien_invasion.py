3dc7cf0140d9285f1a61849a540cf600b8f8d5f7
Illia Sukhadolets <vega334@gmail.com>
2023-09-28 14:15:46 +0300

    add: deleting old bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,6 +28,11 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+            # Delete bullets out of range.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <=0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
             self._update_screen()
 
     def _check_events(self):
@@ -59,8 +64,9 @@ class AlienInvasion:
             self.ship.moving_left = False
     def _fire_bullet(self):
         """ Create bullet and add to sprite Group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """ Update screen."""
