a911ce843f367aa2a7f8800447c8056258acc0cf
Illia Sukhadolets <vega334@gmail.com>
2023-09-29 13:58:10 +0300

    new: change spaceship start position to midleft and chane moving to up/down

--- ship.py
+++ ship.py
@@ -9,26 +9,26 @@ class Ship():
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
         # Make and Get space ship image
-        self.image = pygame.image.load("images/ship.bmp")
+        self.image = pygame.image.load("images/ship_left_side.bmp")
         # Make and Get space ship rectangle
         self.rect = self.image.get_rect()
         # Set space ship window midbottom
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.midleft = self.screen_rect.midleft
         # Make and Save center of space ship rectangle
-        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
         
-        self.moving_right = False
-        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """ Update ship position."""
         # Update x, not rect
-        if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.x += self.settings.ship_speed
-        elif self.moving_left and self.rect.left > 0:
-            self.x -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        elif self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
         # Update rect on base self.x
-        self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
