c634613177455ccecc7d4a86145ba64a4eeb5c98
Illia Sukhadolets <vega334@gmail.com>
2023-09-29 14:13:37 +0300

    new: change bulets rectangel

--- settings.py
+++ settings.py
@@ -10,8 +10,8 @@ class Settings():
         self.ship_speed = 1.5
         # Bullet settings
         self.bullet_speed = 1 
-        self.bullet_width = 3 
-        self.bullet_height = 15 
+        self.bullet_width = 15 
+        self.bullet_height = 3 
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
 
