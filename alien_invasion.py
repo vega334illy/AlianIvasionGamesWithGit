716cffa22abcf3c7ab46fbd492f24acfc04f3a5f
Illia Sukhadolets <vega334@gmail.com>
2023-09-29 14:00:53 +0300

    new: change up/down moving in game

--- alien_invasion.py
+++ alien_invasion.py
@@ -40,21 +40,21 @@ class AlienInvasion:
                 self._check_keyup_events(event)
     
     def _check_keydown_events(self, event):
-        if event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_UP:
         # Muve ship on the right.
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
 
     def _check_keyup_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
     
     def _fire_bullet(self):
         """ Create bullet and add to sprite Group."""
