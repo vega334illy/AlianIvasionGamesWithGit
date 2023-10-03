c771bfc5d697366a83a4adf582815cb1c960280e
Illia Sukhadolets <vega334@gmail.com>
2023-10-03 15:09:46 +0300

    new: add alien module

---
+++ alien.py
@@ -0,0 +1,22 @@
+import pygame
+
+from pygame.sprite import sprite
+
+class Alien(Sprite):
+    """ Class of alien."""
+    def __init__(self, ai_game):
+        """ Init alien and take his start position."""
+        super().__init__()
+        self.screen = ai_game.screen
+
+        # Getting image of alien and setting his attribute rect.
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # Each new alien appears in the upper left corner of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # 
+        self.x = float(self.rect.x)
+
