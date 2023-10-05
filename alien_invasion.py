250f571f6485526e6b76b83a2f20767313104842
Illia Sukhadolets <vega334@gmail.com>
2023-10-05 15:40:16 +0300

    add: row of alien fleet.

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,7 +79,15 @@ class AlienInvasion:
         """ Create aliens fleet."""
         # Create alien
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        for alien_number in range(number_aliens_x):
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
 
     def _update_screen(self):
