32ef5e852e7fa0df3f78394042fc7323761f23c6
Illia Sukhadolets <vega334@gmail.com>
2023-10-06 15:20:25 +0300

    add: direction settings for aliens fleet

--- settings.py
+++ settings.py
@@ -16,4 +16,6 @@ class Settings():
         self.bullet_allowed = 3
         # Alien settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10.0
+        self.fleet_direction = 1
 
