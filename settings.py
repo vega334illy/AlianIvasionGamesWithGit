8f712e8e1ba95d687931f00c9bd2665e1b0c6655
Illia Sukhadolets <vega334@gmail.com>
2023-10-06 14:16:54 +0300

    add: to settings alien speed

--- settings.py
+++ settings.py
@@ -14,4 +14,6 @@ class Settings():
         self.bullet_height = 15 
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
+        # Alien settings
+        self.alien_speed = 1.0
 
