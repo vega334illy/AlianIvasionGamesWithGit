b49eb4d1fbe9b846a2477065c42dbc5ba2005693
Illia Sukhadolets <vega334@gmail.com>
2023-10-08 13:35:39 +0300

    add: _check_flee_direction() and _check_fleet_edges() methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -78,6 +78,7 @@ class AlienInvasion:
 
     def _update_aliens(self):
         """ Update the position of all aliens in the fleet."""
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _create_fleet(self):
@@ -105,7 +106,20 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """ """
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
     
+    def _change_fleet_direction(self):
+        """ """
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _update_screen(self):
         """ Update screen."""
         # Get background.
