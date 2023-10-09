4b19d54685f5aa0b2c88e0a37be5fb2f653a9100
Illia Sukhadolets <vega334@gmail.com>
2023-10-09 12:09:23 +0300

    add: new method center_ship()

--- ship.py
+++ ship.py
@@ -33,3 +33,8 @@ class Ship():
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
+    def center_ship(self):
+        """ Places the ship at the modbotom."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
